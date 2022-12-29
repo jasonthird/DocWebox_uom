685a69eafbaf7149f55e0a92e49c365f6270d8d2
jasonthird <imalkotsis@gmail.com>
2022-12-29 14:08:05 +0200

    put the whole login form into a card

--- php/login.php
+++ php/login.php
@@ -7,25 +7,28 @@
 
     </head>
     <body>
-        <div class="container ">
-            <p>
-                Login
-            </p>
-            <form class="mw-10 form-login d-flex justify-content-center flex-column ">
-            <div class="form-group">
-                <label for="exampleInputEmail1">Username</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            </div>
-            <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-            </div>
-            <br>
-
-            <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <div class="container-float">
+            <div class="row">
+                <div class="col-sm-9 col-md-7 col-lg-5 mx-auto ">
+                <div class="card border-0 shadow rounded-3 m-5">
+                    <div class="card-body text-center p-3 m-1 ">
+                        <h3 class="card-title">Login</h3>
+                        <form>
+                        <div class="form-floating m-3">
+                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                            <label for="floatingInput">Username</label>
+                        </div>
+                        <div class="form-floating m-3">
+                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                            <label for="floatingPassword">Password</label>
+                        </div>
+                        <div class="d-grid gap-2 col-6 mx-auto">
+                            <button class="btn btn-primary" type="button">Submit</button>
+                        </div>
+                </div>
+                </div>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
