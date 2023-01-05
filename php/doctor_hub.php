a62c747a75a3abc80a16138392cfd62c8b738c5b
kwnnabal <82805664+kwnnabal@users.noreply.github.com>
2023-01-05 22:18:26 +0200

    added pages
    added most of the pages in their basic structure

---
+++ php/doctor_hub.php
@@ -0,0 +1,80 @@
+
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/login.css">
+
+    </head>
+    <body>
+        <header>
+            <div class="header" class="d-flex justify-content-end">
+                <h3 class="d-flex justify-content-end">
+                <div class="btn-group">
+                <button type="button" class="btn btn-info">Hello, Username</button>
+                <button type="button" class="btn btn-info dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span class="visually-hidden">Toggle Dropdown</span>
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Profile</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Logout</a></li>
+                </ul>
+                </div>
+            </div>
+        </header>
+
+        
+        <div>
+            <div class="card w-75">
+                <div class="card-body">
+                    <h5 class="card-title">Appointment1</h5>
+                    <h5>User1</br>
+                    12:00</br>
+                    10/05/2023</br>
+                    6958412358</h5>
+                    <a href="#" class="btn btn-primary">View Appointment</a>
+                </div>
+            </div>
+            <br>
+            <div class="card w-75">
+                <div class="card-body">
+                    <h5 class="card-title">Appointment2</h5>
+                    <h5>User2</br>
+                    15:00</br>
+                    11/5/2023</br>
+                    6954745231</h5>
+                    <a href="#" class="btn btn-primary">View Appointment</a>
+                </div>
+            </div>
+            <br>
+            <div class="card w-75">
+                <div class="card-body">
+                    <h5 class="card-title">Appointment3</h5>
+                    <h5>User3</br>
+                    11:00</br>
+                    18/01/2023</br>
+                    6945874125</h5>
+                    <a href="#" class="btn btn-primary">View Appointment</a>
+                </div>
+            </div>
+            <br>
+            <div class="card w-75">
+                <div class="card-body">
+                    <h5 class="card-title">Appointment4</h5>
+                    <h5>User4</br>
+                    18:00</br>
+                    23/01/2023</br>
+                    6958745874</h5>
+                    <a href="#" class="btn btn-primary">View Appointment</a>
+                </div>
+            </div>
+        </div>
+        <br>
+
+
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ php/user_profile_edit.php
@@ -0,0 +1,65 @@
+
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/login.css">
+
+    </head>
+    <body>
+    <header>
+            <div class="header" class="d-flex justify-content-end">
+                <h3 class="d-flex justify-content-end">
+                <div class="btn-group">
+                <button type="button" class="btn btn-info">Hello, Username</button>
+                <button type="button" class="btn btn-info dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span class="visually-hidden">Toggle Dropdown</span>
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Profile</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Logout</a></li>
+                </ul>
+                </div>
+            </div>
+        </header>
+        <div class="container-float">
+            <div class="row">
+                <div class="col-sm-9 col-md-7 col-lg-5 mx-auto ">
+                <div class="card border-0 shadow rounded-3 m-5">
+                    <div class="card-body text-center p-3 m-1 ">
+                        <h3 class="card-title">Edit Profile</h3>
+                        <form>
+                        <div class="form-floating m-3">
+                            <input type="text" class="form-control" id="floatingInput" placeholder="username">
+                            <label for="floatingInput">TestUser</label>
+                        </div>
+                        <div class="form-floating m-3">
+                            <input type="email" class="form-control" id="floatingInput" placeholder="email">
+                            <label for="floatingInput">Testemail</label>
+                        </div>
+                        <div class="form-floating m-3">
+                            <input type="text" class="form-control" id="floatingInput" placeholder="telephone">
+                            <label for="floatingInput">TestTelephone</label>
+                        </div>
+                        <div class="form-floating m-3">
+                            <select class="form-control">
+                            <option>--City--</option>
+                                <option>Thessaloniki</option>
+                                <option selected>Athens</option>
+                                <option>Patra</option>
+                                <option>Ioannina</option>
+                            </select>
+                        </div>
+                        <div class="d-grid gap-2 col-6 mx-auto">
+                            <button class="btn btn-primary" type="button">Save Changes</button>
+                        </div>
+                </div>
+                </div>
+        </div>
+
+    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </body>
+</html>
