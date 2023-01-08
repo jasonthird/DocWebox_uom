76b91786071a803c6e9536070b10412d7d4e46b0
kwnnabal <82805664+kwnnabal@users.noreply.github.com>
2023-01-08 21:59:53 +0200

    added abmin_hub

--- php/admin_hub_appointments.php
+++ php/admin_hub_appointments.php
@@ -3,28 +3,16 @@
     <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
         rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" href="../css/doctor_hub.css">
+        <link rel="stylesheet" href="../css/admin_hub.css">
 
     </head>
     <body>
-        <header>
-            <div class="header" class="d-flex justify-content-end">
-                <h3 class="d-flex justify-content-end">
-                <div class="btn-group">
-                <button type="button" class="btn btn-info">Hello, Username</button>
-                <button type="button" class="btn btn-info dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                    <span class="visually-hidden">Toggle Dropdown</span>
-                </button>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Profile</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Logout</a></li>
-                </ul>
-                </div>
-            </div>
-        </header>
 
-        
+        <nav class="nav nav-pills nav-justified">
+            <a class="nav-link active" aria-current="page" href="#">Appointments</a>
+            <a class="nav-link" href="admin_hub_doctors.php">Doctors</a>
+        </nav>
+        <br>
         <div>
             <div class="card w-50">
                 <div class="card-body">
@@ -71,9 +59,6 @@
             </div>
         </div>
         <br>
-
-
-        </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
---
+++ php/admin_hub_doctors.php
@@ -0,0 +1,149 @@
+
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/admin_hub.css">
+
+    </head>
+    <body>
+
+        <nav class="nav nav-pills nav-justified">
+            <a class="nav-link" href="admin_hub_appointments.php">Appointments</a>
+            <a class="nav-link active" aria-current="page" href="#">Doctors</a>
+        </nav>
+        <br>
+        <div>
+        <div class="row">
+            <div class="col-sm-3 ">
+                <div class="card">
+                <img src="../img/profile2.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Doctor test</h5>
+                    <p class="card-text"><h6>cardiology</h6><h6>Thessaloniki</h6></p>
+                    <a href="#" class="btn btn-primary">View Profile</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                <img src="../img/profile2.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Doctor test</h5>
+                    <p class="card-text"><h6>cardiology</h6><h6>Thessaloniki</h6></p>
+                    <a href="#" class="btn btn-primary">View Profile</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                <img src="../img/profile2.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Doctor test</h5>
+                    <p class="card-text"><h6>cardiology</h6><h6>Thessaloniki</h6></p>
+                    <a href="#" class="btn btn-primary">View Profile</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                <img src="../img/profile2.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Doctor test</h5>
+                    <p class="card-text"><h6>cardiology</h6><h6>Thessaloniki</h6></p>
+                    <a href="#" class="btn btn-primary">View Profile</a>
+                </div>
+                </div>
+            </div>
+            </div>
+            <br>
+            <div class="row">
+            <div class="col-sm-3 ">
+                <div class="card">
+                <img src="../img/profile2.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Doctor test</h5>
+                    <p class="card-text"><h6>cardiology</h6><h6>Thessaloniki</h6></p>
+                    <a href="#" class="btn btn-primary">View Profile</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                <img src="../img/profile2.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Doctor test</h5>
+                    <p class="card-text"><h6>cardiology</h6><h6>Thessaloniki</h6></p>
+                    <a href="#" class="btn btn-primary">View Profile</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                <img src="../img/profile2.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Doctor test</h5>
+                    <p class="card-text"><h6>cardiology</h6><h6>Thessaloniki</h6></p>
+                    <a href="#" class="btn btn-primary">View Profile</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                <img src="../img/profile2.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Doctor test</h5>
+                    <p class="card-text"><h6>cardiology</h6><h6>Thessaloniki</h6></p>
+                    <a href="#" class="btn btn-primary">View Profile</a>
+                </div>
+                </div>
+            </div>
+            </div>
+            <br>        <div class="row">
+            <div class="col-sm-3 ">
+                <div class="card">
+                <img src="../img/profile2.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Doctor test</h5>
+                    <p class="card-text"><h6>cardiology</h6><h6>Thessaloniki</h6></p>
+                    <a href="#" class="btn btn-primary">View Profile</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                <img src="../img/profile2.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Doctor test</h5>
+                    <p class="card-text"><h6>cardiology</h6><h6>Thessaloniki</h6></p>
+                    <a href="#" class="btn btn-primary">View Profile</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                <img src="../img/profile2.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Doctor test</h5>
+                    <p class="card-text"><h6>cardiology</h6><h6>Thessaloniki</h6></p>
+                    <a href="#" class="btn btn-primary">View Profile</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                <img src="../img/profile2.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Doctor test</h5>
+                    <p class="card-text"><h6>cardiology</h6><h6>Thessaloniki</h6></p>
+                    <a href="#" class="btn btn-primary">View Profile</a>
+                </div>
+                </div>
+            </div>
+            </div>
+            <br>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </body>
+</html>
