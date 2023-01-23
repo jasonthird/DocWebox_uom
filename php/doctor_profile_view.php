96b8953416f40e963c66a63dd674700cca9144c5
jasonthird <imalkotsis@gmail.com>
2023-01-24 00:37:08 +0200

    fix bug in doctor profile view

--- php/doctor_profile_view.php
+++ php/doctor_profile_view.php
@@ -1,13 +1,13 @@
 <?php 
 session_start();
-if(!isset($_SESSION["username"])){
-    header("Location: login.php");
-    exit();
-}
-if($_SESSION["type"] != 'doctor'){
-    header("Location: login.php");
-    exit();
-}
+// if(!isset($_SESSION["username"])){
+//     header("Location: login.php");
+//     exit();
+// }
+// if($_SESSION["type"] != 'doctor'){
+//     header("Location: login.php");
+//     exit();
+// }
 ?>
 <html>
     <head>
