a84ee921214b8074e48b1318ba9c81a67ae5314a
jasonthird <imalkotsis@gmail.com>
2023-01-24 00:45:01 +0200

    fix session bug

--- php/doctor_profile_view.php
+++ php/doctor_profile_view.php
@@ -1,9 +1,9 @@
 <?php 
 session_start();
-// if(!isset($_SESSION["username"])){
-//     header("Location: login.php");
-//     exit();
-// }
+if(!isset($_SESSION["username"])){
+    header("Location: login.php");
+    exit();
+}
 // if($_SESSION["type"] != 'doctor'){
 //     header("Location: login.php");
 //     exit();
--- php/user_profile_edit.php
+++ php/user_profile_edit.php
@@ -4,7 +4,7 @@ if(!isset($_SESSION["username"])){
     header("Location: login.php");
     exit();
 }
-if($_SESSION["type"] != 'patient'){
+if($_SESSION["type"] == 'patient'){
     header("Location: login.php");
     exit();
 }
