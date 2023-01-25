10961d9ed45680a65d7321d61712abb2c79fe087
jasonthird <imalkotsis@gmail.com>
2023-01-26 01:29:22 +0200

    Added live search for doctors in userhub

--- php/user_hub.php
+++ php/user_hub.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <?php 
 session_start();
 if(!isset($_SESSION["username"])){
@@ -11,6 +12,7 @@ if($_SESSION["type"] != 'patient'){
 ?>
 <html>
     <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
         rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="../css/user_hub.css">
@@ -42,6 +44,7 @@ if($_SESSION["type"] != 'patient'){
           mysqli_close($conn);               
         ?> 
       </nav>
+      <input type="text" name="searchbox" id="searchbox" class="filterinput form-control" placeholder="search here">
       <br>
       <?php
           include 'connect.php';
@@ -84,6 +87,19 @@ if($_SESSION["type"] != 'patient'){
                 mysqli_close($conn);
             }
       ?>
+
+      <script>
+        //live search
+        $(document).ready(function() {
+            $("#searchbox").on("keyup", function() {
+                var value = $(this).val().toLowerCase();
+                $(".card").filter(function() {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+        });
+</script>
+
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
