f83255d555de03625812906b67a590f613c85262
JustALemon <83575784+AlphaLemon104@users.noreply.github.com>
2023-01-27 18:07:32 +0200

    code cleanup

--- php/doctor_hub.php
+++ php/doctor_hub.php
@@ -18,55 +18,43 @@
 
     </head>
     <body>
-    <header>
-            <?php
-                
-    //    session_start();
-        include 'connect.php';
-        if(isset($_GET['app']))
-                {
-                    $app=$_GET['app'];
-                    $delete= mysqli_query($conn, "DELETE from appointments WHERE id = $app");
-                }
-
-                echo '<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-                <div class="container-fluid">
-                  <a class="navbar-brand" href="#">
-                      <img src="../img/logo.png" width="200" height="80" class="d-inline-block align-top" alt="">
-                  </a>
-                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                      <li class="nav-item">
-                        <a>Hello,' . $_SESSION["username"] .'</a>
-                      </li>
-                      <li class="nav-item active">
-                        <a href="doctor_hub.php">My Appointments</a>
-                      </li> 
-                      <li class="nav-item active">
-                        <a href="doctor_profile_edit.php">Profile</a>
-                      </li>  
-                      <li class="nav-item active">
-                        <a href="nukeSession.php">Logout</a>
-                      </li> 
-                      </ul>
-                    </div>
-                  </div>
-                </nav>';
-        
-            
-              mysqli_close($conn);
-    
-           ?> 
-        
-        <br>
-        </header>
         <?php
-        
+          include 'connect.php';
+          if(isset($_GET['app'])){
+            $app=$_GET['app'];
+            $delete= mysqli_query($conn, "DELETE from appointments WHERE id = $app");
+          }
+          echo '<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="../img/logo.png" width="200" height="80" class="d-inline-block align-top" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a>Hello,' . $_SESSION["username"] .'</a>
+                </li>
+                <li class="nav-item active">
+                  <a href="doctor_hub.php">My Appointments</a>
+                </li> 
+                <li class="nav-item active">
+                  <a href="doctor_profile_edit.php">Profile</a>
+                </li>  
+                <li class="nav-item active">
+                  <a href="nukeSession.php">Logout</a>
+                </li> 
+                </ul>
+              </div>
+            </div>
+          </nav>';
+        mysqli_close($conn);
+      ?> 
+      <br>
+      <?php
         include 'connect.php';
-        
         $id=$_SESSION["id"];
         $query = "SELECT p.FirstName, p.SureName, a.date, a.time, a.id FROM appointments a join profile p where doc_id= $id and patient_id=p.user_id";
         $result = mysqli_query($conn, $query);
@@ -75,25 +63,24 @@
           #keep the appointment once 
           echo '<div class="d-flex flex-wrap">';
           while($row = $result->fetch_assoc()){ 
-              echo '<div class="col-sm-3 doctors">
-                      <div class="blog-item m-1">
-                        <div class="card">
-                          <div class="card-body">
-                          <h4 class="card-title">Appointment ' . $row['id'] . '</h4>
-                          <h6>' . $row['FirstName'] . ' ' .   $row['SureName'] . '</h6>
-                          <h6>' . $row['date'] . '</h6>
-                          <h6>' . $row['time']  .'</h6>
-                          </div>
+            echo '<div class="col-sm-3 doctors">
+                    <div class="blog-item m-1">
+                      <div class="card">
+                        <div class="card-body">
+                        <h4 class="card-title">Appointment ' . $row['id'] . '</h4>
+                        <h6>' . $row['FirstName'] . ' ' .   $row['SureName'] . '</h6>
+                        <h6>' . $row['date'] . '</h6>
+                        <h6>' . $row['time']  .'</h6>
                         </div>
                       </div>
-                    </div>';
+                    </div>
+                  </div>';
           }
           echo '</div>';
           mysqli_close($conn);
         }
     ?>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
--- php/user_appointment.php
+++ php/user_appointment.php
@@ -18,83 +18,79 @@ if($_SESSION["type"] != 'patient'){
     </head>
     <body>
     <?php
-            // session_start();
-            include 'connect.php';
-              echo '<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-                <div class="container-fluid">
-                  <a class="navbar-brand" href="#">
-                      <img src="../img/logo.png" width="200" height="80" class="d-inline-block align-top" alt="">
-                  </a>
-                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                      <li class="nav-item">
-                        <a>Hello,' . $_SESSION["username"] .'</a>
-                      </li>
-                      <li class="nav-item active">
-                        <a href="user_hub.php">Doctors</a>
-                      </li>
-                      <li class="nav-item active">
-                        <a href="user_profile_edit.php">Profile</a>
-                      </li>
-                      <li class="nav-item active">
-                        <a href="user_appointment.php">My appointments</a>
-                      </li>
-                      <li class="nav-item active">
-                        <a href="nukeSession.php">Logout</a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </nav>';            
-        ?> 
-
-        <?php
-        
-        include 'connect.php';
-        
-        if(isset($_GET['id']))
-        {
-          $app=$_GET['id'];
-          $delete= mysqli_query($conn, "DELETE from appointments WHERE id = $app");
-                
-        }
-    
-        $id = $_SESSION["id"];
-        $query = "SELECT a.id as app_id, doc_id, FirstName, SureName, PhoneNumber, Address, date, time  FROM appointments a join profile p where doc_id=p.user_id and patient_id = $id ";
-        $result = mysqli_query($conn, $query);
-        #check if user exists
-        if (mysqli_num_rows($result) > 0) {
-                #keep the appointment once 
-                echo '<div class="d-flex flex-wrap">';
-                while($row = $result->fetch_assoc()){ 
-                    echo '<div class="col-sm-3 doctors">
-                            <div class="blog-item m-1">
-                              <div class="card">
-                                <img src="../img/profile.png" class=" doctor-profile-pic card-img-top">
-                                <div class="card-body">
-                                <h4 class="card-title">' . $row['FirstName']  ." ". $row['SureName'] . '</h4>
-                                <h6>' . $row['PhoneNumber'] . '</h6>
-                                <h6>' . $row['Address'] . '</h6>
-                                <h6>' . $row['date']  .'</h6>
-                                <h6>' . $row['time']  .'</h6>
-                                <a id="doc_profile" href="doctor_profile_view.php?id=' . $row['doc_id'] . '">View Doctor Profile</a>
-                                <div class="text-center">
-                                  <a href="user_appointment_edit.php?id='.$row['app_id'].'" class="btn btn-primary">Edit</a>
-                                  <a href="user_appointment.php?id='.$row['app_id'].'" class="btn btn-primary">Delete</a>
-                                </div>
-                                </div>
+      include 'connect.php';
+        echo '<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="../img/logo.png" width="200" height="80" class="d-inline-block align-top" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a>Hello,' . $_SESSION["username"] .'</a>
+                </li>
+                <li class="nav-item active">
+                  <a href="user_hub.php">Doctors</a>
+                </li>
+                <li class="nav-item active">
+                  <a href="user_profile_edit.php">Profile</a>
+                </li>
+                <li class="nav-item active">
+                  <a href="user_appointment.php">My appointments</a>
+                </li>
+                <li class="nav-item active">
+                  <a href="nukeSession.php">Logout</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>';            
+    ?> 
+    <?php
+      include 'connect.php';
+      if(isset($_GET['id']))
+      {
+        $app=$_GET['id'];
+        $delete= mysqli_query($conn, "DELETE from appointments WHERE id = $app");
+              
+      }
+  
+      $id = $_SESSION["id"];
+      $query = "SELECT a.id as app_id, doc_id, FirstName, SureName, PhoneNumber, Address, date, time  FROM appointments a join profile p where doc_id=p.user_id and patient_id = $id ";
+      $result = mysqli_query($conn, $query);
+      #check if user exists
+      if (mysqli_num_rows($result) > 0) {
+              #keep the appointment once 
+              echo '<div class="d-flex flex-wrap">';
+              while($row = $result->fetch_assoc()){ 
+                  echo '<div class="col-sm-3 doctors">
+                          <div class="blog-item m-1">
+                            <div class="card">
+                              <img src="../img/profile.png" class=" doctor-profile-pic card-img-top">
+                              <div class="card-body">
+                              <h4 class="card-title">' . $row['FirstName']  ." ". $row['SureName'] . '</h4>
+                              <h6>' . $row['PhoneNumber'] . '</h6>
+                              <h6>' . $row['Address'] . '</h6>
+                              <h6>' . $row['date']  .'</h6>
+                              <h6>' . $row['time']  .'</h6>
+                              <a id="doc_profile" href="doctor_profile_view.php?id=' . $row['doc_id'] . '">View Doctor Profile</a>
+                              <div class="text-center">
+                                <a href="user_appointment_edit.php?id='.$row['app_id'].'" class="btn btn-primary">Edit</a>
+                                <a href="user_appointment.php?id='.$row['app_id'].'" class="btn btn-primary">Delete</a>
+                              </div>
                               </div>
                             </div>
-                          </div>';
-                }
-                echo '</div>';
-            }
-              mysqli_close($conn);
+                          </div>
+                        </div>';
+              }
+            echo '</div>';
+          }
+        mysqli_close($conn);
     ?>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
 </html>
